2a744c4f392ff04a13fd66e632c8bcb9a907c7c9
Cad1Lak <86372391+Cad1Lak@users.noreply.github.com>
2021-07-04 23:29:28 +0300

    Upgrade code 2.0

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -2,59 +2,50 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
 
 public class HomeTest {
     WebDriver wd;
-
+    WebDriverWait wait;
+    Random random;
 
     @BeforeMethod
     public void precondition() {
         wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        random = new Random();
+        wait = new WebDriverWait(wd, 10);
         wd.manage().window().maximize();
         wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+        Random random = new Random();
+
 
     }
 
     @Test
     public void findElement() {
 
-        try {
-            wd.findElement(By.xpath("//a[normalize-space()='LOGIN']")).click();
-            wd.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("TestIdea@gmail.com");
-            wd.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("Qwerty123$" + Keys.TAB + Keys.ENTER);
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        wd.findElement(By.xpath("//a[normalize-space()='LOGIN']")).click();
+        wd.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("TestIdea@gmail.com");
+        wd.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("Qwerty123$" +Keys.TAB + Keys.ENTER);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='ADD']")));
 
-        {
-        try {
-            wd.findElement(By.xpath("//a[normalize-space()='ADD']")).click();
-            wd.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("P3b" + Keys.TAB);
-            wd.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("live" + Keys.TAB);
-            wd.findElement(By.xpath("//input[@placeholder='Phone']")).sendKeys("0533987452" + Keys.TAB);
-            wd.findElement(By.xpath("//input[@placeholder='email']")).sendKeys("Test001@gmail.com" + Keys.TAB);
-            wd.findElement(By.xpath("//input[@placeholder='Address']")).sendKeys("0vo2z" + Keys.TAB);
-            wd.findElement(By.xpath("//input[@placeholder='description']")).sendKeys("wmCPH134" + Keys.TAB);
-            wd.findElement(By.xpath("//b[normalize-space()='Save']")).click();
-                Thread.sleep(1000);
-        } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-        wd.findElement(By.xpath("//div[@class='contact-item_card__2SOIM']")).click();
-        wd.findElement(By.xpath("//button[normalize-space()='Remove']")).click();
-        wd.findElement(By.xpath("//button[normalize-space()='Sign Out']")).click();
+        int r = random.nextInt(100) + 1;
+        wd.findElement(By.xpath("//a[normalize-space()='ADD']")).click();
+        wd.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("VASYA" + r + Keys.TAB);
+        wd.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("PUPKIN" + Keys.TAB);
+        wd.findElement(By.xpath("//input[@placeholder='Phone']")).sendKeys("05339874" + r  + Keys.TAB);
+        wd.findElement(By.xpath("//input[@placeholder='email']")).sendKeys("Test" + r + "@gmail.com" + Keys.TAB);
+        wd.findElement(By.xpath("//input[@placeholder='Address']")).sendKeys("0vo2z" + Keys.TAB);
+        wd.findElement(By.xpath("//input[@placeholder='description']")).sendKeys("wmCPH134" + Keys.TAB);
+        wd.findElement(By.xpath("//b[normalize-space()='Save']")).click();
 
 
     }
