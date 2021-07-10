57187558b3e6f308c119c69d9aa1083fc3015d80
Cad1Lak <86372391+Cad1Lak@users.noreply.github.com>
2021-07-10 17:57:47 +0300

    First refactoring code 1.0

--- src/test/java/LoginTestContact.java
+++ src/test/java/LoginTestContact.java
@@ -1,9 +1,6 @@
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -27,45 +24,65 @@ public class LoginTestContact {
         wait = new WebDriverWait(wd, 20);
         wd.manage().window().maximize();
         wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
-        Random random = new Random();
     }
 
     @Test //(enabled = false)
     public void loginTest() {
 
-        wd.findElement(By.xpath("//a[normalize-space()='LOGIN']")).click();
-        wd.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("TestIdea@gmail.com");
-        wd.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("Qwerty123$" + Keys.TAB + Keys.ENTER);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='ADD']")));
+        //Login
+        click(By.xpath("//a[normalize-space()='LOGIN']"));
+        fl(By.xpath("//input[@placeholder='Email']"),"TestIdea@gmail.com");
+        fl(By.xpath("//input[@placeholder='Password']"),"Qwerty123$");
+        click(By.xpath("//button[.=' Login']"));
+        setWait(By.xpath("//a[normalize-space()='ADD']"));
 
+        //createContact
         int r = random.nextInt(100) + 1;
-        wd.findElement(By.xpath("//a[normalize-space()='ADD']")).click();
-        wd.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("VASYA" + r);
-        wd.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("PUPKIN");
-        wd.findElement(By.xpath("//input[@placeholder='Phone']")).sendKeys("05339874" + r);
-        wd.findElement(By.xpath("//input[@placeholder='email']")).sendKeys("Test" + r + "@gmail.com");
-        wd.findElement(By.xpath("//input[@placeholder='Address']")).sendKeys("0vo2z");
-        wd.findElement(By.xpath("//input[@placeholder='description']")).sendKeys("wmCPH134");
-        wd.findElement(By.xpath("//b[normalize-space()='Save']")).click();
+        click(By.xpath("//a[normalize-space()='ADD']"));
+        fl(By.xpath("//input[@placeholder='Name']"),"VASYA" + r);
+        fl(By.xpath("//input[@placeholder='Last Name']"), "PUPKIN");
+        fl(By.xpath("//input[@placeholder='Phone']"),"05339874" + r);
+        fl(By.xpath("//input[@placeholder='email']"),"Test" + r + "@gmail.com");
+        fl(By.xpath("//input[@placeholder='Address']"),"Dom Kolotushkina ");
+        fl(By.xpath("//input[@placeholder='description']"),"wmCPH134");
+        click(By.xpath("//b[normalize-space()='Save']"));
 
+        //Delete contact
+        setWait(By.cssSelector(".contact-item_card__2SOIM"));
+        click(By.cssSelector(".contact-item_card__2SOIM"));
+        click(By.xpath("//button[2]"));
+
+        //Sign Out
+        //click(By.xpath("//button[.='Sign Out']"));
     }
-    @Test
+    @Test (enabled = false)
     public void negativeLogin() {
 
         try {
-            wd.findElement(By.cssSelector("[href='/login']")).click();
-            wd.findElement(By.cssSelector("button:nth-child(4)")).click();
-            Thread.sleep(5000);
+           click(By.cssSelector("[href='/login']"));
+           click(By.cssSelector("button:nth-child(4)"));
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         wd.switchTo().alert().accept();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[text()='Login Failed with code 400']")));
+        setWait(By.xpath("//div[text()='Login Failed with code 400']"));
         String error = wd.findElement(By.xpath("//div[text()='Login Failed with code 400']")).getText();
         Assert.assertEquals(error,("Login Failed with code 400"));
 
 
 
+    }
+    public void click(By locator){
+        wd.findElement(locator).click();
+    }
+    public void fl(By locator, String text){
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+    public void setWait(By locator){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
     @AfterMethod
@@ -74,8 +91,7 @@ public class LoginTestContact {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        } finally {
-            //wd.quit();
+            wd.quit();
         }
 
     }
